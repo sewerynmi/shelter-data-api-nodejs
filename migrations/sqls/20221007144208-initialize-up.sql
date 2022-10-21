ae31234136a009d1e328fb67efd4554837f9d633
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2022-10-21 12:24:42 +0100

    Add locations migration

--- migrations/sqls/20221007144208-initialize-up.sql
+++ migrations/sqls/20221007144208-initialize-up.sql
@@ -9,6 +9,17 @@ CREATE TABLE homelessness.homelessness (
   homeless_relief_duty INT NULL,
   PRIMARY KEY (year, period, location_id)) ENGINE=InnoDB CHARSET=utf8;
 
+CREATE TABLE homelessness.locations (
+  location_id VARCHAR(45) NOT NULL,
+  location_name VARCHAR(100) NOT NULL,
+  location_population INT NULL,
+  PRIMARY KEY (location_id)) ENGINE=InnoDB CHARSET=utf8;
+
 
 -- INSERT INTO homelessness.homelessness (year, period, location_id, location_name, total_init, total_oprd, threatened, homeless_relief_duty) VALUES (2022, 'Jan-March', 'loc1', 'ENGLAND', 1999, 1888, 1777, 1666);
 -- INSERT INTO homelessness.homelessness (year, period, location_id, location_name, total_init, total_oprd, threatened, homeless_relief_duty) VALUES (2022, 'Jan-March', 'loc2', 'London', 223, 223, 556, 234);
+
+
+INSERT INTO homelessness.locations (location_id, location_name, location_population) VALUES 
+  ("E06000001", 'Hartlepool', 92300),
+  ("E08000032", 'Bradford', 546400);
